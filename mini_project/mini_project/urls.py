27aba586fcdd6b5517ec5868ea5b0cac50d5b227
Arbaz <addewalaarbaz1@gmail.com>
2021-04-09 22:10:55 +0530

    Home_Page And Upload_Page Added

--- mini_project/mini_project/urls.py
+++ mini_project/mini_project/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from apptest.views import test_page,upload_image
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', test_page,name="home_page"),
+    path('up/', upload_image,name="upload_image"),
 ]
